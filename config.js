3fd3fde4a978ee267ca0c08f9e8f9d49e4556893
piaethernest <ukmhomed@gmail.com>
2025-07-20 07:46:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const Config = {
-  rateTukar: 10, // 1 koin = Rp10
-  minimalPenarikan: 100, // Minimal 100 koin = Rp1000
-
-  getInfoPenarikan: function () {
-    const rupiah = this.minimalPenarikan * this.rateTukar;
-    return `💡 Penarikan minimal adalah ${this.minimalPenarikan} koin (Rp${rupiah.toLocaleString("id-ID")}).`;
-  }
+  minimalPenarikan: 1000,
+  rateTukar: 1,
+  adminResetToken: "vareset2025",
+  iklanAktif: true
 };
