adb12b221c1e397b37c5b931bf95ab39cc178f2e
piaethernest <ukmhomed@gmail.com>
2025-07-22 04:56:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,1 @@
-const CONFIG = {
-  ENDPOINT_PENARIKAN: "https://script.google.com/macros/s/AKfycbzThBQMzMqIt1vLeZntGjyq1_E8S9fiQrl2dkSILZDlHkydvyDoztR5L4h9WZMMrGNN/exec",
-  BATAS_MINIMAL_IDR: 1000,
-
-  STORAGE_KEYS: {
-    NAMA: "user",
-    COINS: "coins",
-    XP: "xp",
-    LEVEL: "level",
-    IDR: "idr"
-  }
-};
+endpointPenarikan: "https://script.google.com/macros/s/AKfycb.../exec"
