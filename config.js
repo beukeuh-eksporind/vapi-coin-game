317ea1a3c41b008ed887642e4c62a81173474780
piaethernest <ukmhomed@gmail.com>
2025-07-22 04:45:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,12 @@
-// ====== CONFIG GLOBAL VAPICOIN ======
-
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbzThBQMzMqIt1vLeZntGjyq1_E8S9fiQrl2dkSILZDlHkydvyDoztR5L4h9WZMMrGNN/exec",
-
-  XP_PER_TAP: 10,
-  COIN_PER_TAP: 20,
-  IDR_PER_TAP: 5, // setiap klik dapat 5 IDR
-
-  IDR_MIN_TARIK: 1000,
-
-  BONUS_HARIAN: 50,
-  BONUS_HARUS_TUNAIKAN: 200,
+  ENDPOINT_PENARIKAN: "https://script.google.com/macros/s/AKfycbzThBQMzMqIt1vLeZntGjyq1_E8S9fiQrl2dkSILZDlHkydvyDoztR5L4h9WZMMrGNN/exec",
+  BATAS_MINIMAL_IDR: 1000,
 
   STORAGE_KEYS: {
-    user: "user",
-    coins: "coins",
-    xp: "xp",
-    level: "level",
-    idr: "idr",
-    lastBonus: "lastBonus",
-    loginTime: "loginTime"
+    NAMA: "user",
+    COINS: "coins",
+    XP: "xp",
+    LEVEL: "level",
+    IDR: "idr"
   }
 };
